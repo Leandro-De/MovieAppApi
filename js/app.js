866eb37d3b7a05f06992c65d779a488e1380ff16
Yesid Varela <yleandrovarela@gmail.com>
2021-10-26 15:04:44 -0500

    Call Api TMDB

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const API_KEY = "api_key=fca026edba55620a077ed3bc436bfe11";
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+getMovie(API_URL);
+
+function getMovie(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      showMovies(data.results);
+    });
+}
+
+function showMovies(data) {
+  data.forEach((movie) => {
+    const { title, vote_average, poster_path, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+        <img src="${IMG_URL + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+        `;
+  });
+}
+
+function getColor(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
