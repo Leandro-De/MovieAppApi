67789230933065fe4747ca73f25da502a3fcfbbe
Yesid Varela <yleandrovarela@gmail.com>
2021-10-26 15:20:47 -0500

    Search movie works

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const API_KEY = "api_key=fca026edba55620a077ed3bc436bfe11";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
 const IMG_URL = "https://image.tmdb.org/t/p/w500/";
+const searchURL = `${BASE_URL}/search/movie?${API_KEY}`;
 
 const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 getMovie(API_URL);
 
@@ -33,7 +36,7 @@ function showMovies(data) {
             ${overview}
         </div>
         `;
-
+    //Insertar los elementos de la api en el html
     main.appendChild(movieEl);
   });
 }
@@ -47,3 +50,12 @@ function getColor(vote) {
     return "red";
   }
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+  if (searchTerm) {
+    getMovie(searchURL + "&query=" + searchTerm);
+  }
+});
