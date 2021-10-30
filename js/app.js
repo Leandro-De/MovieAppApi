58e27ff7b6f82c08c35c20bbd5346e70f784703c
Yesid Varela <yleandrovarela@gmail.com>
2021-10-30 15:53:01 -0500

    Add pagination

--- js/app.js
+++ js/app.js
@@ -89,6 +89,18 @@ const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 const tagsEl = document.getElementById("tags");
+
+//Paginacion
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const current = document.getElementById("current");
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = "";
+var totalPages = 100;
+
 var selectedGenre = [];
 
 setGenere();
@@ -157,11 +169,30 @@ function clearBtn() {
 getMovie(API_URL);
 
 function getMovie(url) {
+  lastUrl = url;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       if (data.results.length !== 0) {
         showMovies(data.results);
+        currentPage = data.page;
+        nextPage = currentPage + 1;
+        prevPage = currentPage - 1;
+        totalPages = data.total_pages;
+
+        current.innerText = currentPage;
+
+        if (currentPage <= 1) {
+          prev.classList.add("disable");
+          next.classList.remove("disable");
+        } else if (currentPage >= totalPages) {
+          prev.classList.remove("disable");
+          next.classList.add("disable");
+        } else {
+          prev.classList.remove("disable");
+          next.classList.remove("disable");
+        }
+        tagsEl.scrollIntoView({ behavior: "smooth" });
       } else {
         main.innerHTML = `<h1 class="no-result">No Results Found</h1>`;
       }
@@ -217,3 +248,32 @@ form.addEventListener("submit", (e) => {
     getMovie(API_URL);
   }
 });
+
+prev.addEventListener("click", () => {
+  if (prevPage > 0) {
+    pageCall(prevPage);
+  }
+});
+
+next.addEventListener("click", () => {
+  if (nextPage <= totalPages) {
+    pageCall(nextPage);
+  }
+});
+
+function pageCall(page) {
+  let urlSplit = lastUrl.split("?");
+  let queryParams = urlSplit[1].split("&");
+  let key = queryParams[queryParams.length - 1].split("=");
+  if (key[0] != "page") {
+    let url = lastUrl + "&page=" + page;
+    getMovie(url);
+  } else {
+    key[1] = page.toString();
+    let a = key.join("=");
+    queryParams[queryParams.length - 1] = a;
+    let b = queryParams.join("&");
+    let url = urlSplit[0] + "?" + b;
+    getMovie(url);
+  }
+}
